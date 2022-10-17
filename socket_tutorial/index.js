59d4f25b40cc16c9fa1a704149f519ca267069bf
itsuki-jp <itsuki19601@gmail.com>
2022-10-17 22:55:51 +0900

    tutorial The web framework

---
+++ socket_tutorial/index.js
@@ -0,0 +1,15 @@
+// Express initializes app to be a function handler that you can supply to an HTTP server(as seen in line4)
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// We define a route handler `/` that gets called when we hit out website home.
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world</h1>');
+});
+
+// We make the http server listen on port 3000
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
---
+++ socket_tutorial/memo.md
@@ -0,0 +1,14 @@
+1. package.jsonを書く
+```json
+{
+  "name": "socket-chat-example",
+  "version": "0.0.1",
+  "description": "my first socket.io app",
+  "dependencies": {}
+}
+```
+2. npm install express@4
+3. node index.js でサーバーを立ち上げる。
+   1. <a hred="http://localhost:3000">http://localhost:3000</a>
+   2. 
+4. 
